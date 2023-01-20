289ee519b134c5e2e0eca721aec19c2fd2d2878b
Dom Long <dominic.long.au@gmail.com>
2023-01-20 19:08:21 +1100

    Randomise initial word set

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -5,31 +5,30 @@ import '../styles/Grid.css'
 
 function Board() {
   const [key, setKey] = useState([])
+  const [startingTeam, setStartingTeam] = useState()
+  shuffleArray(wordList)
   const words = wordList.slice(0,25)
 
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+  
   const generateNewBoardKey = () => {
     const newKey = [ ...Array(8).fill(1), ...Array(8).fill(2), ...Array(7).fill(0), 3, Math.floor(Math.random() * 2 + 1) ]
-
-    function shuffleArray(array) {
-      for (let i = array.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [array[i], array[j]] = [array[j], array[i]];
-      }
-    }
-
+    setStartingTeam(newKey[24])
     shuffleArray(newKey)
-
     setKey(newKey)
   }
 
   if (!key.length) {
     generateNewBoardKey()
   }
-
-  console.log(key)
     
   return (
-    <Grid words={words} boardKey={key}/>
+    <Grid words={words} boardKey={key} startingTeam={startingTeam}/>
   );
 }
 
--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -3,6 +3,13 @@ import '../styles/Grid.css'
 
 function Grid(props) {
 
+  const categoryMap = {
+    0: 'neutral',
+    1: 'teamA',
+    2: 'teamB',
+    3: 'assassin'
+}
+
   const cards = props.words.map((x,i) => {
     return (
     <Card key={i} word={x} category={props.boardKey[i]}/>
@@ -10,7 +17,7 @@ function Grid(props) {
   })
     
   return (
-    <div className="grid">
+    <div className={`grid ${categoryMap[props.startingTeam]}`}>
         {cards}
     </div>
   );
