6789679412af9b1f60a3c4635e5c981825088af0
Dom Long <dominic.long.au@gmail.com>
2023-06-21 19:57:49 +1000

    fix build script

--- build_repos.sh
+++ build_repos.sh
@@ -4,4 +4,4 @@ echo "Build script"
 
 npm install --prefix ./backend ./backend &
 npm install --prefix ./frontend ./frontend && npm run build --prefix ./frontend ./frontend
-npm start --prefix ./backend
+# npm start --prefix ./backend
