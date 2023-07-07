72e186f8d05230eac9b0b3bf8a45193fe203ed28
Dom Long <dominic.long.au@gmail.com>
2023-07-08 01:16:36 +1000

    take 2

--- build_repos.sh
+++ build_repos.sh
@@ -2,6 +2,6 @@
 
 echo "Build script"
 
-npm install --prefix ./backend ./backend && npm run tsc --prefix ./backend
+npm install --prefix ./backend ./backend && npm run tsc --prefix ./backend &
 npm install --prefix ./frontend ./frontend && npm run build --prefix ./frontend ./frontend
 # npm start --prefix ./backend
