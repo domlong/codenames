cda0d52ac16026e4c2c85c45926720c706670d61
Dom Long <dominic.long.au@gmail.com>
2023-07-08 02:22:02 +1000

    letsgo

--- build_repos.sh
+++ build_repos.sh
@@ -2,6 +2,18 @@
 
 echo "Build script"
 
-npm install --prefix ./backend ./backend && npm run tsc --prefix ./backend &
-npm install --prefix ./frontend ./frontend && npm run build --prefix ./frontend ./frontend
+# npm install --prefix ./backend ./backend && npm run tsc --prefix ./backend &
+# npm install --prefix ./frontend ./frontend && npm run build --prefix ./frontend ./frontend
 # npm start --prefix ./backend
+
+echo "Installing backend..."
+npm install --prefix ./backend ./backend
+
+echo "Compiling backend..."
+npm run tsc --prefix ./backend
+
+echo "Installing frontend..."
+npm install --prefix ./frontend ./frontend
+
+echo "Building frontend..."
+npm run build --prefix ./frontend ./frontend
